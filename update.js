5a7932e90879cec31a2bea96bf20a7a5f9a5aafb
Fu Cheng <alexcheng1982@gmail.com>
2018-11-21 08:01:03 +1300

    Add PHP 7.1

---
+++ update.js
@@ -0,0 +1,33 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+const Handlebars = require('handlebars');
+
+function downloadFile(url, filename) {
+  execSync(`curl ${url} -o ${filename} -s`);
+}
+
+const version = process.argv[2];
+const config = JSON.parse(fs.readFileSync(`./${version}/update.json`).toString());
+
+const {
+  path,
+  files,
+  lines: {
+    start,
+    end,
+  }
+} = config;
+
+const baseUrl = 'https://raw.githubusercontent.com/docker-library/php'
+
+files.forEach(file => {
+  downloadFile(`${baseUrl}/${path}/${file}`, `${version}/${file}`)
+});
+
+const template = Handlebars.compile(fs.readFileSync(`./${version}/Dockerfile.hbs`).toString(), { noEscape: true });
+const lines = fs.readFileSync(`./${version}/Dockerfile`).toString().split('\n');
+const content = lines.slice(start, end).join('\n');
+const result = template({ content });
+fs.writeFileSync(`./${version}/Dockerfile`, result);
+
+execSync(`chmod +x ${version}/docker-php-*`)
